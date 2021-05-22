4130b52ae45485363c8554f725bd93190e492bf3
saimahesh1279 <saimahesh1279@gmail.com>
2021-05-22 14:09:55 +0530

    1

--- AngularGmailBox/src/app/app.module.ts
+++ AngularGmailBox/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComposemailComponent } from './composemail/composemail.component';
+import { ComposeMailComponent } from './compose-mail/compose-mail.component';
+import { MailBoxComponent } from './mail-box/mail-box.component';
+import { SendMailComponent } from './send-mail/send-mail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComposemailComponent,
+    ComposeMailComponent,
+    MailBoxComponent,
+    SendMailComponent
   ],
   imports: [
     BrowserModule,
---
+++ AngularGmailBox/src/app/mail-box/mail-box.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MailBoxComponent } from './mail-box.component';
+
+describe('MailBoxComponent', () => {
+  let component: MailBoxComponent;
+  let fixture: ComponentFixture<MailBoxComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MailBoxComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MailBoxComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
